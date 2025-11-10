2fd681e95cf3974fe5c97eb898e251bbf6239c81
mubarak584569-lang <mubarak584569@gmail.com>
2025-11-10 10:28:10 +0000

    Update SevenSegment.java

--- coursework1/SevenSegment.java
+++ coursework1/SevenSegment.java
@@ -1,9 +1,9 @@
 public class SevenSegment {
-    static String ssd(int d, int n) { 
+   String ssd(int d, int n) { 
     switch ((d*10)+n) { 
         case  1: case  5: case 21: case 23: case 25: case 31: case 33: case 35: 
         case 43: case 51: case 53: case 55: case 61: case 63: case 65: case 71: 
-        case 81: case 83: case 85: case 91: case 93: case 95: 
+        case 81: case 83: case 85: case 91: case 93: case 95:  
             return " --  "; 
         case 24: case 52: case 62: 
             return "|    "; 
@@ -20,7 +20,7 @@ static void display(int number) {
     int len = numberString.length();
 
     for(int line = 1; line <= 5; line++){
-        String row = " ";
+        String row = "";
 
         for (int i = 0; i < len; i++){
              int digit = numberString.charAt(i) - '0';
